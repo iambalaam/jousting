b60fff3f6343dded693aefc837b0872b2c86bdd6
Guy Balaam <guybal@amazon.com>
2019-08-03 01:59:56 +0100

    Adding vaulting

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,13 @@ const CANVAS_HEIGHT = 400;
 
 let BG_COLOR;
 
+const PLAYER_SIZE = 10;
 const GRAVITY = 0.5;
 const ACCELERATION = 0.3;
 const MAX_SPEED = 6;
 const AIR_MANEUVERABILITY = 0.2;
-const SWORD_LENGTH = 5;
+const SWORD_LENGTH = 10;
+const FLOOR_HEIGHT = 100
 
 let floor;
 let player;
@@ -15,10 +17,10 @@ function setup() {
     BG_COLOR = color(89, 124, 66);
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    floor = createSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT + 50, CANVAS_WIDTH, 120);
+    floor = createSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT - FLOOR_HEIGHT / 2, CANVAS_WIDTH, FLOOR_HEIGHT);
     floor.shapeColor = BG_COLOR;
 
-    player = createSprite(CANVAS_WIDTH / 5, CANVAS_HEIGHT - 30, 10, 10);
+    player = createSprite(CANVAS_WIDTH / 5, CANVAS_HEIGHT - FLOOR_HEIGHT - 20, PLAYER_SIZE, PLAYER_SIZE);
     player.shapeColor = color(10, 10, 10);
 }
 
@@ -37,29 +39,47 @@ function draw() {
         pointer = { x: touches[0].x, y: touches[0].y }
     }
 
-    // Movement
+    // Sword
+    let swordTip;
     if (pointer) {
+        const vel = Math.hypot(player.velocity.x, player.velocity.y);
+        const swordLength = Math.sqrt(vel) * SWORD_LENGTH;
+        const angle = mouseX > player.position.x
+            ? Math.atan((player.position.y - mouseY) / (mouseX - player.position.x))
+            : Math.atan((player.position.y - mouseY) / (mouseX - player.position.x)) + Math.PI;
+        swordTip = {
+            x: swordLength * Math.cos(angle) + player.position.x,
+            y: - swordLength * Math.sin(angle) + player.position.y
+        }
+        stroke(255);
+        strokeWeight(1);
+        line(
+            player.position.x, player.position.y,
+            swordTip.x, swordTip.y,
+        );
+    }
+
+    // Movement
+    const playerIsOnTheGround = (player.position.y + 1 + PLAYER_SIZE / 2) >= (CANVAS_HEIGHT - FLOOR_HEIGHT)
+    const swordIsInTheGround = swordTip && swordTip.y + 1 >= (CANVAS_HEIGHT - FLOOR_HEIGHT)
+    if (playerIsOnTheGround && pointer && !swordIsInTheGround) {
+        // On the Ground
         if (pointer.x < player.position.x) {
             player.velocity.x = Math.max(-MAX_SPEED, player.velocity.x - ACCELERATION);
         } else {
             player.velocity.x = Math.min(MAX_SPEED, player.velocity.x + ACCELERATION)
         }
-    } else {
+    } else if (playerIsOnTheGround && !pointer) {
+        player.velocity.x *= 0.8;
+    } else if (!playerIsOnTheGround && swordIsInTheGround) {
         player.velocity.x *= 0.8;
     }
 
-    // Sword
-    if (pointer) {
-        const vel = Math.hypot(player.velocity.x, player.velocity.y);
-        const angle = mouseX > player.position.x
-            ? Math.atan((player.position.y - mouseY) / (mouseX - player.position.x))
-            : Math.atan((player.position.y - mouseY) / (mouseX - player.position.x)) + Math.PI;
-        stroke(255);
-        strokeWeight(1);
-        line(
-            player.position.x, player.position.y,
-            vel * SWORD_LENGTH * Math.cos(angle) + player.position.x, -vel * SWORD_LENGTH * Math.sin(angle) + player.position.y,
-        );
+    // Vault
+    if (pointer && swordTip) {
+        if (swordTip.y > (CANVAS_HEIGHT - FLOOR_HEIGHT)) {
+            player.setVelocity(player.velocity.x, 1.1 * ((CANVAS_HEIGHT - FLOOR_HEIGHT) - swordTip.y));
+        }
     }
 
 
@@ -79,6 +99,4 @@ function draw() {
 
     // final draw 
     drawSprites();
-    floor.debug = mouseIsPressed;
-    player.debug = mouseIsPressed;
 }
